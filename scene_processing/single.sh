5534a7b79748057f339cdca3c712be05f1a2c198
Abhishek Anand <aa755@cs.cornell.edu>
2011-08-18 23:53:56 -0400

    changed the single script

--- scene_processing/single.sh
+++ scene_processing/single.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-cp ~/Nips2011/cleanKinectData/office/singles/allBags/*.bag ./
+#cp ~/Nips2011/cleanKinectData/office/singles/allBags/*.bag ./
 num=0
 for file in `dir -d *.bag` ; do
 num=`expr $num + 1`
 cp $file globalTransform.bag
-rosrun scene_processing live_segment_computeFeats &
 #echo "$file $num" >> scene_mapping.txt 
-rosbag play -d 10 globalTransform.bag
-sleep 600
-killall live_segment_computeFeats
-killall svm_python_classify
+rosbag play -d 20 globalTransform.bag &
+rosrun scene_processing live_segment_computeFeats 
+
 mkdir $file.pred
 mv *.pcd $file.pred/
 mv pred.* $file.pred/
