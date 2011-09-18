c1f0e464cbd8edfa1d6ab326dff32fff5c07ce86
Abhishek Anand <aa755@cs.cornell.edu>
2011-09-18 16:39:28 -0400

    added offline evaluation code

--- scene_processing/single.sh
+++ scene_processing/single.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 #cp ~/Nips2011/cleanKinectData/office/singles/allBags/*.bag ./
 num=0
-for file in `dir -d *.bag` ; do
+for file in `dir -d keyboard_testing_scenes/*/*.pcd` ; do
 num=`expr $num + 1`
 #if [ $num -gt 6 ]
 # then
-cp $file globalTransform.bag
+#cp $file globalTransform.bag
 #echo "$file $num" >> scene_mapping.txt 
-rosbag play -d 10 globalTransform.bag &
-rosrun scene_processing live_segment_computeFeats > log.txt
+
+echo "processing $file"
+#rosbag play -d 10 globalTransform.bag &
+#rosrun scene_processing live_segment_computeFeats > log.txt
+rosrun scene_processing live_segment_computeFeats office $file abc > log.txt
 
 mkdir $file.pred
 mv *.pcd $file.pred/
