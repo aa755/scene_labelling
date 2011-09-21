3a06fdbe883cf26dbfbfeb1a60a5ed4dc92625b2
Hema Koppula <hema@hemaHPUbuntu.(none)>
2011-09-21 00:45:03 -0400

    added binary pcd to ply converter

--- scene_processing/src/includes/point_types.h
+++ scene_processing/src/includes/point_types.h
@@ -7,9 +7,9 @@
 
 #ifndef POINT_TYPES_H
 #define	POINT_TYPES_H
- #include <pcl/point_types.h>
- #include <pcl/point_cloud.h>
- #include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/io/pcd_io.h>
 #include <pcl/filters/impl/extract_indices.hpp>
 #include <pcl/filters/impl/passthrough.hpp>
 #include <pcl/kdtree/kdtree.h>
@@ -22,59 +22,63 @@
 #include "pcl/features/normal_3d.h"
 #include "pcl/features/impl/normal_3d.hpp"
 
-namespace pcl
-{
-    struct PointXYGRGBCam
-    {
+namespace pcl {
+
+    struct PointXYGRGBCam {
         PCL_ADD_POINT4D;
-       float rgb;
+        float rgb;
         PCL_ADD_NORMAL4D;
-       uint32_t cameraIndex;
-       float distance;
-             float curvature;
+        uint32_t cameraIndex;
+        float distance;
+        float curvature;
+
+    };
+    
+    struct PointPLY {
+        PCL_ADD_POINT4D;
+        uint32_t r;
+        uint32_t g;
+        uint32_t b;
 
     };
 
-    struct PointXYInt
-    {
+    struct PointXYInt {
         int x;
         int y;
         int z;
         int segment;
         int label;
     };
-    
-    struct PointXYZRGBCamSL
-    {
+
+    struct PointXYZRGBCamSL {
         PCL_ADD_POINT4D;
-  union
-  {
-    struct
-    {
-      float rgb;
-    };
-    float data_c[4];
-  };
-       uint32_t cameraIndex;
-       float distance;
-       uint32_t segment;
-       uint32_t label;
-       
- //      inline PointXYZRGBCamSL (float _x, float _y, float _z) { x = _x; y = _y; z = _z; data[3] = 1.0f; }
-       
-       void clone(const struct PointXYZRGB &rhs)
-       {
-           for(int i=0;i<4;i++)
-               data[i]=rhs.data[i];
-           rgb=rhs.rgb;
-           segment=0;
-           label=0;
-           cameraIndex=0;
-           distance=0.0;
-           
-       }
-         EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-} EIGEN_ALIGN16;
+
+        union {
+
+            struct {
+                float rgb;
+            };
+            float data_c[4];
+        };
+        uint32_t cameraIndex;
+        float distance;
+        uint32_t segment;
+        uint32_t label;
+
+        //      inline PointXYZRGBCamSL (float _x, float _y, float _z) { x = _x; y = _y; z = _z; data[3] = 1.0f; }
+
+        void clone(const struct PointXYZRGB & rhs) {
+            for (int i = 0; i < 4; i++)
+                data[i] = rhs.data[i];
+            rgb = rhs.rgb;
+            segment = 0;
+            label = 0;
+            cameraIndex = 0;
+            distance = 0.0;
+
+        }
+        EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    } EIGEN_ALIGN16;
 }
 
 POINT_CLOUD_REGISTER_POINT_STRUCT(
@@ -96,6 +100,17 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(
         (int, label, label)
         )
         
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+        pcl::PointPLY,
+        (float, x, x)
+        (float, y, y)
+        (float, z, z)
+        (int, r, r)
+        (int, g, g)
+        (int, b, b)
+        )
+        
+
 POINT_CLOUD_REGISTER_POINT_STRUCT(
         pcl::PointXYZRGBCamSL,
         (float, x, x)
